089f37d0e5420736ea61308cd1cf179d46d33cdc
Atul Mishra <atul.mishra@temenos.com>
2022-08-29 20:08:48 +0530

    First Git Commt

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,30 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoTest {
+	
+	@Test
+	public void sum() {
+		int a = 20;
+		int b = 10;
+		int SUM = a+b;
+		System.out.println(SUM);
+	}
+	@Test
+	public void sub() {
+		int a = 20;
+		int b = 10;
+		int SUB = a-b;
+		System.out.println(SUB);
+	}
+	@Test
+	public void multi() {
+		int a = 20;
+		int b = 10;
+		int MULTI = a*b;
+		System.out.println(MULTI);
+	}
+}
